be4bb0dfc68e3d78d4ae7b24dceeff2ce7721424
Miji5 <Miji5@emirim>
2022-04-18 11:01:09 +0900

    함수없이 최소값 구하기

--- CodingTest/02 배열활용연습.c
+++ CodingTest/02 배열활용연습.c
@@ -4,7 +4,7 @@
 void getMax(int* arr, int length)
 {
 	int temp = -1;	// 비교대상
-	int idx = 99;	// 인덱스 값
+	int idx = 999;	// 인덱스 값
 	
 	for (int i = 0; i < length; i++)
 	{
@@ -23,5 +23,19 @@ int main(void)
 	int arr1[10] = { 8, 5, 1, 6, 4, 9, 10, 7, 11, 14 };
 	getMax(arr1, 10);
 
+	// 함수 쓰지 말고 최소값과 그 idx를 구해보자
+	int temp = 999; // 비교대상
+	int idx = 999;	// 인덱스 값
+
+	for (int i = 0; i < sizeof(arr1)/sizeof(arr1[0]); i++)
+	{
+		if (arr1[i] < temp)
+		{
+			temp = arr1[i];
+			idx = i;
+		}
+	}
+	printf("최소값은:%d 인덱스 값은:%d \n", temp, idx);
 	return 0;
+
 }
