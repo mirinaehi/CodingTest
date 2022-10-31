f382166747f412ac98879936af83fc4ffa3d511b
Miji5 <Miji5@emirim>
2022-10-31 10:58:58 +0900

    02-04 메인 알고리즘 작성

---
+++ CodingTest/기출문제/02-04.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// words 그냥 문자열이 들어가 있는 것
+// 5개이상의 문자열만 걸러서 넣는다
+char* solution(char* words[], int words_len) {
+    char* answer = "";
+    
+    // 길이5이상의 문자열만 answer에 넣을거다
+    for(int i=0; i<words_len; i++)
+        if (strlen(words[i]) >= 5)
+            strcat(answer, words[i]);
+
+
+    return answer;
+}
+
+int main() {
+    char* words1[5] = { "my", "favorite", "color", "is", "violet" };
+    int words_len1 = 5;
+    char* ret1 = solution(words1, words_len1);
+
+    printf("solution 함수의 반환 값은 %s 입니다.\n", ret1);
+
+    char* words2[3] = { "yes", "i", "am" };
+    int words_len2 = 3;
+    char* ret2 = solution(words2, words_len2);
+
+    printf("solution 함수의 반환 값은 %s 입니다.\n", ret2);
+}
